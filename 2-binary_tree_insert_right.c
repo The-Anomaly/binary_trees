2fe46a82570cfa5b374f746a12d287d00d7327f6
The-Anomaly <favouradekogbe@gmail.com>
2022-08-26 10:03:10 +0000

    2-binary_tree_insert_right.c update

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,6 +1,6 @@
 #include "binary_trees.h"
 
-/*
+/**
  * binary_tree_insert_right - inserts a node as the right child of another node
  * @parent: pointer to the node to insert the right child
  * @value: value to store in the new node
@@ -12,18 +12,18 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *node = NULL;
 
-        if (parent == NULL)
-                return (NULL);
+	if (parent == NULL)
+		return (NULL);
 
-        node = binary_tree_node(parent, value);
-        if (parent == NULL)
-                return (NULL);
+	node = binary_tree_node(parent, value);
+	if (parent == NULL)
+		return (NULL);
 
-        if (parent->right)
-        {
-                node->right = parent->right;
-                node->right->parent = node;
-        }
-                parent->right = node;
-        return (node);
+	if (parent->right)
+	{
+		node->right = parent->right;
+		node->right->parent = node;
+	}
+	parent->right = node;
+	return (node);
 }
