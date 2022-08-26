44f7af97feab20c15c1e099da3f33cc9e87885fe
The-Anomaly <favouradekogbe@gmail.com>
2022-08-26 23:58:31 +0000

    100-binary_trees_ancestor.c update

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -9,11 +9,12 @@
  * Return: pointer to the lowest common ancestor node, otherwise NULL
  *
  */
-binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+const binary_tree_t *second)
 {
 	binary_tree_t *tmp;
 
-	if(!first || !second)
+	if (!first || !second)
 		return (NULL);
 
 	tmp = (binary_tree_t *)second;
